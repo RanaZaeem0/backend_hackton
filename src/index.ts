d560ded828408b81e2d422bf67e3b764ad519b3d
Rana Zaeem <ranazaeem34@gmail.com>
2025-01-26 04:58:56 +0500

    ddd porrt

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const startServer = async () => {
     try {
         await connectDB();
         
-        const PORT =   8000;
+        const PORT = process.env.PORT  8000;
         app.listen(PORT, () => {
             console.log(`Server running on port ${PORT}`);
         });
